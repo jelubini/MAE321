9e7594e1e989938c5664ec4d91c5016b0e2db7b8
Joel Lubinitsky <jelubini@syr.edu>
2015-02-03 02:06:47 -0500

    Finished HW3_1 and HW3_2

---
+++ HW3_2.m
@@ -0,0 +1,53 @@
+% Joel Lubinitsky - 02/04/15
+% MAE 321 - HW 3.2
+
+clear all
+close all
+clc
+
+% Problem 2:
+% Springs are available in stiffness values of 10, 100, and 1000 N/m.
+% Design a spring system using these values only, so that a 150 kg mass is
+% connected to ground with an undamped natural frequency of about 1.5 rad/s.
+
+% Unknown
+%   Number/position of springs
+
+%% Known
+mass             = 150;  % kg
+frequencyNatural = 1.5;  % rad/s
+stiffness1       = 10;   % kg/s^2
+stiffness2       = 100;  % kg/s^2
+stiffness3       = 1000; % kg/s^2
+stiffness        = [stiffness1 stiffness2 stiffness3];
+
+%% Calculations
+% w_n = sqrt(k/m) -> k = m * w_n ^ 2
+stiffnessTotal = mass * frequencyNatural ^ 2;
+
+stiffness = sort(stiffness, 'descend');
+nSpringsParallel = zeros(1, length(stiffness));
+stiffnessRemaining = stiffnessTotal;
+for k = stiffness
+    
+    if k > stiffnessRemaining
+        continue
+    end
+    
+    indexSpring = find(k == stiffness);
+    nSpringsParallel(indexSpring) = floor(stiffnessRemaining / k);
+    stiffnessRemaining = stiffnessRemaining - k * nSpringsParallel(indexSpring);
+end
+
+nSpringsSeries = zeros(1, length(stiffness));
+[minSpring, indexSpring] = min(stiffness);
+for n = [1 : 1 : 10]
+    nSpringsSeries(n, indexSpring) = ceil(minSpring / stiffnessRemaining);
+    stiffnessRemaining = stiffnessRemaining - minSpring / nSpringsSeries(n, indexSpring);
+    
+    if stiffnessRemaining == 0
+        break
+    end
+    
+    nSpringsSeries = [nSpringsSeries; zeros(1, length(stiffness))];
+end
