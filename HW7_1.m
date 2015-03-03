eb27af1f016bd224942ff39c5b40f3b693398426
Joel Lubinitsky <jelubini@syr.edu>
2015-03-03 04:11:39 -0500

    Group Project Source Summary

---
+++ HW7_1.m
@@ -0,0 +1,16 @@
+% Joel Lubinitsky
+% MAE 231 - HW7.1
+% 03/04/15
+
+clear all
+close all
+clc
+
+%% Problem 1:
+% An $$ 85 kg $$ mass is suspended by a spring of stiffness $$ 20 \times 103 \
+% N/m $$ with a viscous damping constant of $$ 1200 Ns/m $$. The mass is
+% initially at rest and in equilibrium. Calculate the steady-state
+% displacement amplitude and phase if the mass is excited by a harmonic
+% force of $$ 75 N $$ at $$ 3 Hz $$.
+%
+% Find: $$ x, \phi $$
